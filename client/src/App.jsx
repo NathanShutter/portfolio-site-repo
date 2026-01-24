c3058f9b2bd56c70bcb44bf7f4d6289832564060
Nathan Shutter <nateshut1@gmail.com>
2026-01-24 16:45:29 -0500

    Reorder sections: Experience before Projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
-import Projects from './components/Projects'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
 import Resume from './components/Resume'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
@@ -14,8 +14,8 @@ function App() {
       <Header />
       <Hero />
       <About />
-      <Projects />
       <Experience />
+      <Projects />
       <Resume />
       <Contact />
       <Footer />
